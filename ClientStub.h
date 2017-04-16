f815e98454bbb2f9bbc85a3c0ab80e7b22880e8c
Dunitt Monagas <dunitt.monagas@example.com>
2017-04-16 01:46:28 -0400

    Estructura básica para peticiones individuales y por lote.

--- ClientStub.h
+++ ClientStub.h
@@ -1,9 +1,59 @@
 
+#include <string>
+#include <iostream>
+
+#include <curl/curl.h>
+#include "json.hpp"
+
+using namespace std;
+using json = nlohmann::json;
+
+typedef struct MemoryStruct{
+
+	char *memory;
+	size_t size;
+
+}MemoryStruct;
+
+typedef enum{
+	INACTIVE= 0,
+	ACTIVE,
+	READY,
+	RECEIVED
+}BacthStage;
 
 class ClientStub{
-	
+
 	public:
-		ClientStub();
+
+		char* url;
+		CURL *curl;
+		bool error;
+		CURLcode res;
+		json request;
+		json response;
+		BacthStage batch;
+		unsigned int port;
+		MemoryStruct chunk;
+		static unsigned int id;
+
+		bool send(const char* str, long size);
+
+	public:
+		
+		ClientStub(const char* url, unsigned int port);
 		~ClientStub();
-	
+		bool isReady();
+		bool isError();
+
+		void beginBatch();
+		void endBatch();
+		int getBatch(unsigned int idx, int *result);
+		int getBatch(unsigned int idx, double *result);
+		int getBatch(unsigned int idx, complex<double> *result);
+		
+		int sum(int a, int b, bool notification);
+		double sum(double a, double b, bool notification);
+
+
 };
--- main.cpp
+++ main.cpp
@@ -1,11 +1,78 @@
 
+#include <iostream>
+#include <cmath>
+#include <complex>
+
+#include <curl/curl.h>
 #include "ClientStub.cpp"
 
 using namespace std;
 
+ClientStub *client;
+
+bool testSUM(int a, int b){
+
+	int reslt= client->sum(a, b);
+
+	if(client->isError()){
+		cout << "Error" << endl;
+		return false;
+	}
+
+	if(reslt != (a + b))
+		cout << reslt << " " << a + b << " ";
+		
+	return reslt == (a + b);
+
+}
+
+bool testSUM(double a, double b){
+
+	double reslt= client->sum(a, b);
+
+	if(client->isError()){
+		cout << "Error" << endl;
+		return false;
+	}
+	
+	if(reslt != (a + b))
+		cout << reslt << " " << a + b << " ";
+
+	return reslt == (a + b);
+
+}
+
+
+
 int main(){
 
+	double result;
+	client= new ClientStub("localhost", 8888);
 	
+	std::cout << std::boolalpha;
+
+	try{
 	
+		client->beginBatch();
+		cout << "*************************************************************************" << endl;
+		cout << "testSUM: " << testSUM(1.1, 5.0) << endl;
+		cout << "*************************************************************************" << endl;
+		cout << "*************************************************************************" << endl;
+		cout << "testSUM: " << testSUM(1.1, 3.0) << endl;
+		cout << "*************************************************************************" << endl;
+		client->endBatch();
+		cout << client->getBatch(0, &result) << endl;
+		cout << "result: "  << result << endl;
+		cout << client->getBatch(1, &result) << endl;
+		cout << "result: "  << result << endl;
+		cout << "testSUM: " << testSUM(1, 3) << endl;
+		//cout << "*************************************************************************" << endl;
+		//cout << "testDIVISION: " << testDIVISION(2, 0) << endl;
+		//cout << "*************************************************************************" << endl;
+	}catch(const char* msg){
+		cout << msg << endl;
+	}
+
 	return 0;
+
 }
